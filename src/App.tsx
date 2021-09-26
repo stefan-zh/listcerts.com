f26a3d99adde4ebdc30b8967bbb8bc1c3153a283
Stefan Zhelyazkov <stefan.zhelyazkov@gmail.com>
2021-09-26 16:47:42 +0300

    Add type information for certificates

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,43 @@ import './App.css';
 interface Certificate {
   subject: Name;
   issuer: Name;
+  not_before: Date;
+  not_after: Date;
+  sans: string[];
+  pub_key_info: PublicKey;
+  misc: Miscellaneous;
+  sha256: string;
+  sha1: string;
+  ca: boolean;
+  key_usage: string;
+  extended_key_usages: string[];
+  subject_key_id: string;
+  authority_key_id: string;
+  crl_endpoints: string[];
+  ocsp_server: string[];
+  issuing_cert_url: string[];
 }
 
 interface Name {
-  readonly country: string;
-  readonly state_province: string;
-  readonly locality: string;
-  readonly organization: string;
-  readonly organizational_unit: string;
-  readonly common_name: string;
+  country: string;
+  state_province: string;
+  locality: string;
+  organization: string;
+  organizational_unit: string;
+  common_name: string;
+}
+
+interface PublicKey {
+  algorithm: string;
+  size: number;
+  value: string;
+}
+
+interface Miscellaneous {
+  serial_number: string;
+  signature_algorithm: string;
+  version: number;
+  pem: string;
 }
 
 function App() {
