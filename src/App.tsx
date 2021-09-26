745889eaec230433b12be17e4ad280ebe10bb48d
Stefan Zhelyazkov <stefan.zhelyazkov@gmail.com>
2021-09-26 17:54:29 +0300

    Use nav tabs and tab panes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, {ChangeEvent, FormEvent, useState} from 'react';
-import {Alert, Button, Container, Form, InputGroup, Nav, Navbar} from "react-bootstrap";
+import {Alert, Button, Container, Form, InputGroup, Nav, Navbar, Tab} from "react-bootstrap";
 import {Certificate} from './data';
 import './App.css';
+import {InfoGroup} from "./InfoGroup";
 
 export const App = () => {
   const [url, setUrl] = useState<string>("");
@@ -21,7 +22,7 @@ export const App = () => {
       setCerts([]);
       setErr(await response.text());
     } else {
-      const certsJson: {certs: Certificate[]} = await response.json();
+      const certsJson: { certs: Certificate[] } = await response.json();
       setCerts(certsJson.certs);
       setErr("");
     }
@@ -46,16 +47,17 @@ export const App = () => {
       <Container>
         <Form onSubmit={onSubmit}>
           <InputGroup className="search">
-            <Form.Control size="lg" type="text" onChange={onInput}  placeholder="https://www.example.com" />
+            <Form.Control size="lg" type="text" onChange={onInput} placeholder="https://www.example.com"/>
             <InputGroup.Text>
               <Button id="submit" variant="light" type="submit">Search</Button>
             </InputGroup.Text>
           </InputGroup>
         </Form>
         {err &&
-          <Alert variant="danger">{err}</Alert>
+        <Alert variant="danger">{err}</Alert>
         }
-        {certs &&
+        {certs.length > 0 &&
+        <Tab.Container defaultActiveKey="cert-0">
           <Nav justify variant="tabs" defaultActiveKey="cert-0">
             {certs.map((cert, idx) => (
               <Nav.Item key={idx}>
@@ -63,6 +65,14 @@ export const App = () => {
               </Nav.Item>
             ))}
           </Nav>
+          <Tab.Content>
+            {certs.map((cert, idx) => (
+              <Tab.Pane key={idx} eventKey={'cert-' + idx}>
+                <h2>{cert.subject.common_name}</h2>
+              </Tab.Pane>
+            ))}
+          </Tab.Content>
+        </Tab.Container>
         }
       </Container>
     </>
